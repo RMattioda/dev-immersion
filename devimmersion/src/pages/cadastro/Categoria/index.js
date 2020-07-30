35f87da7ad2a88733dd66691b54152555e834e20
RMattioda <rodrigo-mattioda@hotmail.com>
2020-07-30 16:53:39 -0300

    adicionando eslint, prop-types, json-server, concurrently e melhorando formulário de cadastro de categoria

--- devimmersion/src/pages/cadastro/Categoria/index.js
+++ devimmersion/src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,113 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
+function CadastroCategoria() {
+  const valoresInicias = {
+    nome: '',
+    descricao: '',
+    cor: '#000',
+  };
 
-function CadastroCategoria(){
-    return (
-        <PageDefault>
-            <h1>Aqui cadastramos categoria</h1>
-            <Link to="/">
-                ir para home
-            </Link>
-        </PageDefault>
+  const [categorias, setCategorias] = useState([]);
+
+  const [values, setValues] = useState(valoresInicias);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  useEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategorias(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
+  }, []);
+
+  function handleChange(infosDoCampo) {
+    const { value } = infosDoCampo.target;
+    setValue(
+      infosDoCampo.target.getAttribute('name'),
+      value,
     );
+  }
+
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(informacaoDoEvento) {
+        informacaoDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+
+        setValues(valoresInicias);
+      }}
+      >
+        <div>
+
+          <FormField
+            label="Nome da Categoria"
+            type="text"
+            name="nome"
+            value={values.nome}
+            onChange={handleChange}
+          />
+
+          <FormField
+            label="Descrição"
+            type="textarea"
+            name="descricao"
+            value={values.descricao}
+            onChange={handleChange}
+          />
+
+          <FormField
+            label="Cor"
+            type="color"
+            name="cor"
+            value={values.cor}
+            onChange={handleChange}
+          />
+        </div>
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        ir para home
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
